c96dd7774528786eaf6da8d5a363953b34f357af
fede2796 <fede27396@gmail.com>
2022-06-23 14:12:53 +0200

    Fine esercizio

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// BONUS:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data (in data avrò l'src).
+
+var app = new Vue({
+    el: '#root',
+    data: {
+      text: "Vincenzo Nibali",
+      image: "https://www.oasport.it/wp-content/uploads/2022/05/19399951_small-640x350.jpg.webp"
+    }
+  })
